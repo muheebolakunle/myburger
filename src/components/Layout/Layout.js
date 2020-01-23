a49baa4b537fed70eb3ad5d629d46444628c16dd
Muheeb Olakunle <olakunlemuheeb@gmail.com>
2020-01-23 13:56:04 +0100

    implement logo component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 import styles from './Layout.module.css';
 
 const layout = (props) => {
     return (
         <Aux>
-            <div>
-                Toolbar,SideDrawer,Backdrop
-            </div>
+            <Toolbar />
 
             <main className={styles.content}>
                 {props.children}
